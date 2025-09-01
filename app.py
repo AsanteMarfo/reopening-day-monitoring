38715bdafdd947b6ee7d6a3a2eb7536119747e4f
Asante Marfo Charles <77259308+AsanteMarfo@users.noreply.github.com>
2025-09-01 13:23:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=ad7250f2-9da9-4090-a07a-fbc873aca868&autoAuth=true&ctid=95f31300-13cc-44d5-93da-e2a04daacd7d"
+powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=ad7250f2-9da9-4090-a07a-fbc873aca868&autoAuth=true&ctid=95f31300-13cc-44d5-93da-e2a04daacd7d&chromeless=1&navContentPaneEnabled=false"
 
 st.header("Monitoring Dashboard")
 st.write("""
@@ -16,9 +16,9 @@ st.write("""
         This Dashboard helps you monitor live data submission from field officers and School Improvement Officers.
     """)
 # Replace with your actual embed URL
-powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=ad7250f2-9da9-4090-a07a-fbc873aca868&autoAuth=true&ctid=95f31300-13cc-44d5-93da-e2a04daacd7d"
+powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=ad7250f2-9da9-4090-a07a-fbc873aca868&autoAuth=true&ctid=95f31300-13cc-44d5-93da-e2a04daacd7d&chromeless=1&navContentPaneEnabled=false"
 
 # Embed the dashboard
-components.iframe(src=powerbi_url, width=1000, height=700, scrolling=True)
+components.iframe(src=powerbi_url, width=100%, height=700, scrolling=True)
 
 #st.image("https://app.powerbi.com/reportEmbed?reportId=ad7250f2-9da9-4090-a07a-fbc873aca868&autoAuth=true&ctid=95f31300-13cc-44d5-93da-e2a04daacd7d", caption="Re-Opening Day Monitoring", use_container_width=True)
