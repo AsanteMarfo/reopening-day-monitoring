bf9fe4c30b31abcf7d8061cc56fca08d96f6e16b
Asante Marfo Charles <77259308+AsanteMarfo@users.noreply.github.com>
2025-09-01 12:34:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=ad7250f2-9da9-4090-a
 
 st.header("Monitoring Dashboard")
 st.write("""
-             Welcome to the **Re-Opening Monitoring Dashbord** application!
+        Welcome to the **Re-Opening Monitoring Dashbord** application!
         This Dashboard helps you monitor live data submission from field officers and School Improvement Officers.
     """)
-    # Replace with your actual embed URL
-    powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=fee59b6f-0e86-46ec-9fb7-c3d788880ae8&autoAuth=true&ctid=95f31300-13cc-44d5-93da-e2a04daacd7d"
+# Replace with your actual embed URL
+powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=fee59b6f-0e86-46ec-9fb7-c3d788880ae8&autoAuth=true&ctid=95f31300-13cc-44d5-93da-e2a04daacd7d"
 
 # Embed the dashboard
-    components.iframe(src=powerbi_url, width=1000, height=700, scrolling=True)
+components.iframe(src=powerbi_url, width=1000, height=700, scrolling=True)
 
 #st.image("https://app.powerbi.com/reportEmbed?reportId=ad7250f2-9da9-4090-a07a-fbc873aca868&autoAuth=true&ctid=95f31300-13cc-44d5-93da-e2a04daacd7d", caption="Re-Opening Day Monitoring", use_container_width=True)
